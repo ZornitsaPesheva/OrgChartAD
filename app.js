ebfa9a3d5dd5ab5a01584708048f86c0a3c80359
unknown <zorry@AD.BALKANGRAPH.COM>
2019-11-04 11:24:55 +0000

    user authenticated

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var ActiveDirectory = require('activedirectory');
+var config = { url: 'ldap://ad.balkangraph.com',
+               baseDN: 'dc=balkangraph,dc=com',
+               username: 'zorry@ad.balkangraph.com',
+               password: 'qaz123wsx!@#' }
+var ad = new ActiveDirectory(config);
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+var ad = new ActiveDirectory(config);
+var username = 'zorry@ad.balkangraph.com';
+var password = 'qaz123wsx!@#';
+ 
+ad.authenticate(username, password, function(err, auth) {
+  if (err) {
+    console.log('ERROR: '+JSON.stringify(err));
+    return;
+  }
+  
+  if (auth) {
+    console.log('Authenticated!');
+  }
+  else {
+    console.log('Authentication failed!');
+  }
+});
