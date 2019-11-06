dbcdf00ee6d114630cedda5b870c28e56c251ca8
unknown <zorry@AD.BALKANGRAPH.COM>
2019-11-06 11:12:02 +0000

    slinks added

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const ad = require('./ad');
 ad.getGroupsWithMembership(function(err, tree){
     
     addId(tree);
+    var slinks = sLinks(tree);
+
    
-    addPid(tree);
+  //  addPid(tree);
 
     console.log(tree);
+    console.log(slinks);
 });
   
 function addId(tree) {
@@ -21,15 +24,38 @@ function addId(tree) {
 
 }
 
-function addPid(tree) {
+// function addPid(tree) {
+//     for (i = 0; i < tree.length; i++) {
+//         if (tree[i].list.length > 0) {
+//             for (j = 0; j < tree[i].list.length; j++) {
+//                 var group = tree[i].list[j];
+//                   for (k = 0; k < tree.length; k++){
+//                       if (tree[k].name == group) tree[i].pid = tree[k].id;
+//                   }
+//             }
+//         }
+//     }
+
+// }
+
+function sLinks(tree) {
+    var slinks = [];
     for (i = 0; i < tree.length; i++) {
         if (tree[i].list.length > 0) {
             for (j = 0; j < tree[i].list.length; j++) {
                 var group = tree[i].list[j];
                   for (k = 0; k < tree.length; k++){
-                      if (tree[k] == group) tree[i].pid = tree[k].id;
+                      if (tree[k].name == group) {
+
+                        var link = {};
+                        link.from = tree[i].id;
+                        link.to = tree[k].id;
+
+                        slinks.push(link);
+                      }
                   }
             }
         }
     }
+    return slinks;
 }
